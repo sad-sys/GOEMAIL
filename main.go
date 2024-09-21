84d801fdbca4cd2cb4065f5716b6990238617027
sad-sys <khawajasadiq25@gmail.com>
2024-09-21 15:21:44 +0100

    Added the ability to listen on go

--- main.go
+++ main.go
@@ -17,4 +17,40 @@ func main() {
 	defer listener.Close()
 
 	fmt.Println("Listening on port 25...")
+
+	for {
+		// Accept an incoming connection
+		conn, err := listener.Accept()
+
+		if err != nil {
+			log.Printf("Error accepting connection %v", err)
+			continue
+		}
+		//Handle connection in a new goroutine
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	// Send SMTP greeting
+	_, err := conn.Write([]byte("220 Welcome to Go SMTP Listener Sadiq.com \r\n"))
+	if err != nil {
+		log.Printf("Error writing to connection: %v", err)
+		return
+	}
+
+	// Read data from the client
+	buffer := make([]byte, 1024)
+	for {
+		n, err := conn.Read(buffer)
+		if err != nil {
+			log.Printf("Error reading from connection: %v", err)
+			return
+		}
+
+		// Echo the data back to the client for now (just for testing)
+		conn.Write(buffer[:n])
+	}
 }
